9be000c48ec574792bbc7577495b07b78409b6d1
Anonymous <>
2025-01-27 13:52:42 -0800

    Dimension changes

---
+++ infer/modules/train/extract_whisper_print.py
@@ -0,0 +1,103 @@
+import os
+import sys
+import traceback
+from einops import rearrange
+
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+os.environ["PYTORCH_MPS_HIGH_WATERMARK_RATIO"] = "0.0"
+
+device = sys.argv[1]
+n_part = int(sys.argv[2]) # number of GPUs
+i_part = int(sys.argv[3]) # index of GPU (for splitting dataset)
+if len(sys.argv) == 7:
+    exp_dir = sys.argv[4]
+    version = sys.argv[5]
+    is_half = sys.argv[6].lower() == "true"
+else:
+    i_gpu = sys.argv[4]
+    exp_dir = sys.argv[5]
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(i_gpu)
+    version = sys.argv[6]
+    is_half = sys.argv[7].lower() == "true"
+import numpy as np
+import soundfile as sf
+import torch
+import torch.nn.functional as F
+
+from svc_helper.sfeatures.models import SVC5WhisperModel
+import torch
+
+if "privateuseone" not in device:
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif torch.backends.mps.is_available():
+        device = "mps"
+else:
+    import torch_directml
+
+    device = torch_directml.device(torch_directml.default_device())
+
+f = open("%s/extract_f0_feature.log" % exp_dir, "a+")
+
+def printt(strr):
+    print(strr)
+    f.write("%s\n" % strr)
+    f.flush()
+
+
+printt(" ".join(sys.argv))
+
+printt("exp_dir: " + exp_dir)
+wavPath = "%s/1_16k_wavs" % exp_dir
+outPath = (
+    "%s/3_feature1280" % exp_dir 
+)
+os.makedirs(outPath, exist_ok=True)
+
+
+# wave must be 16k
+def readwave(wav_path, normalize=False):
+    wav, sr = sf.read(wav_path)
+    assert sr == 16000
+    feats = torch.from_numpy(wav).float()
+    if feats.dim() == 2:  # stereo channels
+        feats = feats.mean(-1)
+    assert feats.dim() == 1, feats.dim()
+    if normalize:
+        with torch.no_grad():
+            feats = F.layer_norm(feats, feats.shape)
+    return feats
+
+
+# Whisper model
+whisper = SVC5WhisperModel()
+
+todo = sorted(list(os.listdir(wavPath)))[i_part::n_part]
+n = max(1, len(todo) // 10)  # 最多打印十条
+if len(todo) == 0:
+    printt("no-feature-todo")
+else:
+    printt("all-feature-%s" % len(todo))
+    for idx, file in enumerate(todo):
+        try:
+            if file.endswith(".wav"):
+                wav_path = "%s/%s" % (wavPath, file)
+                out_path = "%s/%s" % (outPath, file.replace("wav", "npy"))
+
+                if os.path.exists(out_path):
+                    continue
+
+                wave = readwave(wav_path)
+
+                feats = whisper.extract_features(wave) # [seq_len, channels]
+                feats = feats.float().cpu().numpy()
+                if np.isnan(feats).sum() == 0:
+                    np.save(out_path, feats, allow_pickle=False)
+                else:
+                    printt("%s-contains nan" % file)
+                if idx % n == 0:
+                    printt("now-%s,all-%s,%s,%s" % (len(todo), idx, file, feats.shape))
+        except:
+            printt(traceback.format_exc())
+    printt("all-feature-done")
